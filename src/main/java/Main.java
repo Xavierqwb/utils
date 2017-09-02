939298b775a34ef905328d0ea654eeacc89d5266
qiuwenbin <qiuwenbin@youzan.com>
2017-09-02 16:56:03 +0800

    add .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by qiuwenbin on 2017/8/12.
  */
@@ -10,9 +16,8 @@ public class Main {
 	private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		String a = "aaaa", b = "bbb", c = "cc";
-		LOGGER.debug("test debug {}", a);
-		LOGGER.info("test info {}", b);
-		LOGGER.error("test error {}", c);
+		Optional<String> optional = Optional.of("aaa");
+		optional = optional.flatMap((s) -> Optional.of(s + "!"));
+		optional.ifPresent(System.out::println);
 	}
 }
